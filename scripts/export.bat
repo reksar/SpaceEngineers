b06b8180116502c6c374a9bce3e7cb85f394f7cd
reksarka <reksarka@gmail.com>
2021-03-31 11:12:56 +0300

    Exports PNG in addition to CS.

--- scripts/export.bat
+++ scripts/export.bat
@@ -1,52 +1,61 @@
 @ECHO off
-REM This script used as the external tool in Visual Studio to export ingame 
-REM scripts for Space Engineers game.
-REM ARGUMENTS:
-REM     %1 - Project name, or $(TargetName) in Visual Studio
-SET SOURCES_ROOT=E:\reksar\creation\SpaceEngineers\scripts
-SET DESTINATION_ROOT=C:\Users\reksar\AppData\Roaming\SpaceEngineers\IngameScripts\local
-SET SED=E:\reksar\soft\portable\git\usr\bin\sed.exe
-SET STRART_LINE_PATTERN="/^\s*\/\/ INGAME SCRIPT START/="
-SET END_LINE_PATTERN="/^\s*\/\/ INGAME SCRIPT END/="
-SET FILENAME=Script.cs
+REM The `export.bat <script_dir>` cmd exports it to the Space Engineers game.
+SET SED=D:\reksarka\app\run\git\usr\bin\sed.exe
+SET SRC_DIR=D:\reksarka\app\src\SpaceEngineers\scripts
+SET SE_DIR=C:\Users\reksar\AppData\Roaming\SpaceEngineers
+SET SE_SCRIPTS_DIR=%SE_DIR%\IngameScripts\local
+SET START_MARKER_PATTERN="/^\s*\/\/ INGAME SCRIPT START/="
+SET END_MARKER_PATTERN="/^\s*\/\/ INGAME SCRIPT END/="
+SET CS=Script.cs
+SET PNG=thumb.png
 
 
-REM Sanitizes the quoted argument.
-SET project_name=%~1
+REM Sanitizes the quoted <script_dir> argument.
+SET script_dir=%~1
 
-IF [%project_name%] == [] (
-    ECHO C# project name is not specified.
+IF [%script_dir%] == [] (
+    ECHO Script dir is not specified.
     EXIT /B 1
 )
-SET source_file=%SOURCES_ROOT%\%project_name%\%FILENAME%
-IF NOT EXIST %source_file% (
-    ECHO Source file is not found: %source_file%
+
+IF NOT EXIST %SE_DIR% (
+    ECHO Space Engineers AppData is not found: %SE_DIR%
     EXIT /B 2
 )
-IF NOT EXIST %DESTINATION_ROOT% (
-    ECHO Space Engineers destination folder is not found: %DESTINATION_ROOT%
+
+SET src_png=%SRC_DIR%\%script_dir%\%PNG%
+SET src_cs=%SRC_DIR%\%script_dir%\%CS%
+IF NOT EXIST %src_cs% (
+    ECHO Script is not found: %src_cs%
     EXIT /B 3
 )
-SET destination_dir=%DESTINATION_ROOT%\%project_name%
-IF NOT EXIST %destination_dir% MKDIR %destination_dir%
+
+SET dest_dir=%SE_SCRIPTS_DIR%\%script_dir%
+IF NOT EXIST %dest_dir% (
+    MKDIR %dest_dir%
+)
 
 REM Find first and last line of ingame script in the source file, and
 REM save the numbers of these lines.
-SET tmp_file=%destination_dir%\tmp
-%SED% -n %STRART_LINE_PATTERN% %source_file% > %tmp_file%
-SET /P start_line_num= < %tmp_file%
-%SED% -n %END_LINE_PATTERN% %source_file% > %tmp_file%
-SET /P end_line_num= < %tmp_file%
+SET tmp_cs=%dest_dir%\tmp.cs
+%SED% -n %START_MARKER_PATTERN% %src_cs% > %tmp_cs%
+SET /P start_line_num= < %tmp_cs%
+%SED% -n %END_MARKER_PATTERN% %src_cs% > %tmp_cs%
+SET /P end_line_num= < %tmp_cs%
 
-REM Copy ingame part of the source file into Space Engineers game dir.
-%SED% -n "%start_line_num%,%end_line_num%p" %source_file% > %tmp_file%
+REM Copy the ingame script part into the tmp file.
+%SED% -n "%start_line_num%,%end_line_num%p" %src_cs% > %tmp_cs%
 
-REM Remove first code indent (tabulation or 4 spaces) at start of each line.
+REM Remove first indent (tab or 4 spaces) at the start of each line.
 REM Save script into original file.
 SET TAB_STOP=4
-SET script=%destination_dir%\%FILENAME%
-%SED% "s/^\(\s\{%TAB_STOP%\}\|\t\)//" %tmp_file% > %script%
-DEL %tmp_file%
+%SED% "s/^\(\s\{%TAB_STOP%\}\|\t\)//" %tmp_cs% > %dest_dir%\%CS%
+DEL %tmp_cs%
 
-ECHO "%project_name%" script has been exported.
+REM Copy the PNG image.
+IF EXIST %src_png% (
+    COPY %src_png% %dest_dir%\%PNG% 1>NUL
+)
+
+ECHO "%script_dir%" script has been exported.
 EXIT /B 0
