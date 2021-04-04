65da7cf89806214c70f594b183137b039b939334
reksarka <reksarka@gmail.com>
2021-04-04 13:52:55 +0300

    Adapts to VS Code task.
    * Accepts full path as parameter.
    * Resolves spaces in paths.

--- scripts/export.bat
+++ scripts/export.bat
@@ -1,22 +1,22 @@
 @ECHO off
-REM The `export.bat <script_dir>` cmd exports it to the Space Engineers game.
+REM Use `export.bat [full path to script_dir]` to export into the game.
 
 REM Settings
 SET SED=<Path to Git>\usr\bin\sed.exe
-SET SRC_DIR=<Path to current repo>\SpaceEngineers\scripts
 SET SE_DIR=C:\Users\<User>\AppData\Roaming\SpaceEngineers
 
 SET SE_SCRIPTS_DIR=%SE_DIR%\IngameScripts\local
-SET START_MARKER_PATTERN="/^\s*#region Ingame/="
-SET END_MARKER_PATTERN="/^\s*#endregion \/\/ Ingame/="
+SET REGION="/^\s*#region Ingame/="
+SET ENDREGION="/^\s*#endregion \/\/ Ingame/="
 SET CS=Script.cs
 SET PNG=thumb.png
 
 
-REM Sanitizes the quoted <script_dir> argument.
+REM Unquoting the parameter, because the path may be passed both with quotes 
+REM (if it contains spaces) and without.
 SET script_dir=%~1
 
-IF [%script_dir%] == [] (
+IF ["%script_dir%"] == [""] (
     ECHO Script dir is not specified.
     EXIT /B 1
 )
@@ -26,48 +26,56 @@ IF NOT EXIST %SE_DIR% (
     EXIT /B 2
 )
 
-SET src_png=%SRC_DIR%\%script_dir%\%PNG%
-SET src_cs=%SRC_DIR%\%script_dir%\%CS%
-IF NOT EXIST %src_cs% (
+SET src_png=%script_dir%\%PNG%
+SET src_cs=%script_dir%\%CS%
+IF NOT EXIST "%src_cs%" (
     ECHO Script is not found: %src_cs%
     EXIT /B 3
 )
 
-SET dest_dir=%SE_SCRIPTS_DIR%\%script_dir%
-IF NOT EXIST %dest_dir% (
-    MKDIR %dest_dir%
+REM Extract `script_dirname` from `script_dir` full path.
+FOR %%f IN ("%script_dir%") DO SET script_dirname=%%~nxf
+
+SET dest_dir=%SE_SCRIPTS_DIR%\%script_dirname%
+IF NOT EXIST "%dest_dir%" (
+    MKDIR "%dest_dir%" 2>NUL
 )
-
-REM First, it will be used to store SED's out, then - to store the raw script.
-SET tmp_file=%dest_dir%\tmp
-
-REM Find and save the bounds of ingame script region.
-%SED% -n %START_MARKER_PATTERN% %src_cs% > %tmp_file%
-SET /P start_line_num= < %tmp_file%
-SET /A start_line_num=start_line_num+1
-%SED% -n %END_MARKER_PATTERN% %src_cs% > %tmp_file%
-SET /P end_line_num= < %tmp_file%
-SET /A end_line_num=end_line_num-1
-
-REM Copy the ingame script part into the tmp file.
-%SED% -n "%start_line_num%,%end_line_num%p" %src_cs% > %tmp_file%
 IF %ERRORLEVEL% NEQ 0 (
-    ECHO Can not extract the ingame script part.
-    DEL %tmp_file%
-    DEL %dest_dir%\%CS% 2>NUL
+    ECHO Can not resolve path %dest_dir%
     EXIT /B 4
 )
 
-REM Remove first indent (tab or 4 spaces) at the start of each line.
-REM Save script into original file.
-SET TAB_STOP=4
-%SED% "s/^\(\s\{%TAB_STOP%\}\|\t\)//" %tmp_file% > %dest_dir%\%CS%
-DEL %tmp_file%
+REM It will be used to store the line numbers and then the raw ingame script.
+SET tmp=%dest_dir%\tmp
 
-REM Copy the PNG image.
-IF EXIST %src_png% (
-    COPY %src_png% %dest_dir%\%PNG% 1>NUL
+REM Find and save the bounds of `#region Ingame`.
+%SED% -n %REGION% "%src_cs%" > "%tmp%"
+SET /P start_line_num= < "%tmp%"
+SET /A start_line_num=start_line_num+1
+%SED% -n %ENDREGION% "%src_cs%" > "%tmp%"
+SET /P end_line_num= < "%tmp%"
+SET /A end_line_num=end_line_num-1
+
+REM Copy the raw ingame script part.
+%SED% -n "%start_line_num%,%end_line_num%p" "%src_cs%" > "%tmp%"
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Can not extract ingame script part.
+    DEL "%tmp%"
+    DEL "%dest_dir%\%CS%" 2>NUL
+    EXIT /B 5
 )
 
-ECHO "%script_dir%" script has been exported.
+REM Remove first indent (tab or 4 spaces) at the start of each line.
+REM Save the script into the final file.
+SET TAB_STOP=4
+%SED% "s/^\(\s\{%TAB_STOP%\}\|\t\)//" "%tmp%" > "%dest_dir%\%CS%"
+
+DEL "%tmp%"
+
+REM Copy the PNG image.
+IF EXIST "%src_png%" (
+    COPY "%src_png%" "%dest_dir%\%PNG%" 1>NUL
+)
+
+ECHO "%script_dir%" has been exported.
 EXIT /B 0
