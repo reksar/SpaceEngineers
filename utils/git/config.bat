15361998b8e53bd2279499aea1adb456f87e3631
reksarka <reksarka@gmail.com>
2022-11-12 16:17:34 +0200

    FIX: Git filters config

--- utils/git/config.bat
+++ utils/git/config.bat
@@ -1,6 +1,11 @@
-@call ..\config.bat || exit
+setlocal
 
-set git=%GIT_DIR%\bin\git.exe --git-dir=%cd%\..\..\.git
+@call %~dp0..\config.bat || exit
+
+set git_local_dir=%~dp0..\..\.git
+set git="%GIT_DIR%\cmd\git.exe" --git-dir="%git_local_dir%"
 
 %git% config --local filter.unset-git-dir.clean utils/git/unset-git-dir.sh
 %git% config --local filter.reset-se-path.clean utils/git/reset-se-path.sh
+
+endlocal
