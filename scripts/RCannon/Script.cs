ff5267a97e6d32a1aa24ee3dc0c47d64dfa0f4b9
reksarka <reksarka@gmail.com>
2022-11-23 00:14:37 +0200

    [Vision] Basic targets broadcast

--- scripts/RCannon/Script.cs
+++ scripts/RCannon/Script.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 // Space Engineers DLLs
 using Sandbox.ModAPI.Ingame;
 using Sandbox.ModAPI.Interfaces;
 using Sandbox.Game.EntityComponents;
 using VRageMath;
+using VRage;
 using VRage.Game;
 using VRage.Collections;
 using VRage.Game.Components;
@@ -24,24 +26,28 @@ public sealed class Program : MyGridProgram {
 	// Image names that will be displayed on the LCD at the Programming Block keyboard to indicate readiness to Fire.
 	const string READY = "Arrow";
 	const string NOT_READY = "Cross";
+	const string CONSTRUCTION = "Construction";
 
-	IMyTextSurface MeKeyLCD;
+	IMyTextSurface MeLCD, MeKeyLCD;
 
 	List<IMyMotorStator> Spring;
 	List<IMyShipWelder> Welders;
 	IMyShipMergeBlock WarheadMerge;
+
+	// To tracking targets.
+	IMyBroadcastListener Vision;
 
 	// Implements FSM.
 	StateRunner State;
 	delegate void StateRunner(string argument, UpdateType updateSource);
 
 	public Program() {
+
 		Runtime.UpdateFrequency = UpdateFrequency.Update1;
 
 		WarheadMerge = GridTerminalSystem.GetBlockWithName("RCannonMerge") as IMyShipMergeBlock;
-
 		IMyBlockGroup RCannon = GridTerminalSystem.GetBlockGroupWithName("RCannon");
-		
+
 		Spring = Select<IMyMotorStator>(RCannon);
 		Spring.ForEach(rotor => {
 			rotor.Enabled = false;
@@ -52,18 +58,24 @@ public sealed class Program : MyGridProgram {
 			rotor.UpperLimitRad = 0;
 			rotor.TargetVelocityRPM = 60;
 		});
-		
+
 		Welders = Select<IMyShipWelder>(RCannon);
 		Welders.ForEach(welder => {
 			welder.Enabled = false;
 			welder.UseConveyorSystem = true;
 		});
 
+		State = FitSpring;
+
+		Vision = IGC.RegisterBroadcastListener("Vision");
+
+		// LCD will be used to show targets.
+		MeLCD = Me.GetSurface(0);
+		MeLCD.ContentType = VRage.Game.GUI.TextPanel.ContentType.TEXT_AND_IMAGE;
+
 		// Keyboard LCD will be used as indicator.
 		MeKeyLCD = Me.GetSurface(1);
 		MeKeyLCD.ContentType = VRage.Game.GUI.TextPanel.ContentType.TEXT_AND_IMAGE;
-
-		State = FitSpring;
 	}
 
 	public void Main(string argument, UpdateType updateSource) {
@@ -100,7 +112,7 @@ public sealed class Program : MyGridProgram {
 		var Sensor = GridTerminalSystem.GetBlockWithName("RCannonWarheadSensor") as IMySensorBlock;
 
 		if (Sensor == null) {
-			Indicate(NOT_READY);
+			Indicate(CONSTRUCTION);
 			Welders.ForEach(welder => welder.Enabled = true);
 		} else {
 			Indicate(READY);
@@ -110,8 +122,25 @@ public sealed class Program : MyGridProgram {
 	}
 
 	void Ready(string argument, UpdateType updateSource) {
+
+		DisplayTargets();
+
 		if (updateSource == UpdateType.Trigger && argument == "Fire") {
 			State = Fire;
+		}
+	}
+
+	void DisplayTargets() {
+
+		var Targets = Vision.AcceptMessage().As<
+			ImmutableList<
+				MyTuple<
+					MyTuple<long, string, int, Vector3D, bool, MatrixD>,
+					MyTuple<Vector3, int, BoundingBoxD, long>>>>();
+		
+		if (Targets != null) {
+			var Names = Targets.ConvertAll(entity => entity.Item1.Item2);
+			MeLCD.WriteText(String.Join("\n", Names));
 		}
 	}
 
@@ -125,8 +154,8 @@ public sealed class Program : MyGridProgram {
 
 			// If the Warhead is not detonated by Sensor or by damage, it will be self-destruct after
 			Warhead.DetonationTime = 60; // s
-
 			Warhead.StartCountdown();
+
 		} catch {
 			Echo("ERR: Warhead is not found!");
 		}
---
+++ scripts/Vision/Script.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+// Space Engineers DLLs
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Game.EntityComponents;
+using VRageMath;
+using VRage;
+using VRage.Game;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
+
+namespace Vision {
+
+public sealed class Program : MyGridProgram {
+
+	#region Vision
+
+	// Tags for broadcasting Vision `Entities` in the Intergrid Communication system - `MyGridProgram.IGC`.
+	const string IGC_VISION = "Vision";
+	// TODO: third-party sensors, e.g. RayCast.
+	const string IGC_VISION_ADD = "VisionAdd";
+
+	DetectedEntityComparer EntityComparer;
+	HashSet<MyDetectedEntityInfo> ActualEntities;
+	HashSet<MyDetectedEntityInfo> DeprecatedEntities;
+
+	// Temporary storage for blocks selected from the grid when updating vision sources.
+	List<IMyFunctionalBlock> BlockStorage;
+
+	// Sources to recieve info about detected entities.
+	List<Sensor> Sensors;
+
+	// To display `Entities` here, just for debugging purposes.
+	IMyTextSurface MeLCD;
+
+	class DetectedEntityComparer : EqualityComparer<MyDetectedEntityInfo> {
+
+		public override bool Equals(MyDetectedEntityInfo entity1, MyDetectedEntityInfo entity2) {
+			return entity1.EntityId == entity2.EntityId;
+		}
+
+		public override int GetHashCode(MyDetectedEntityInfo entity) {
+			return entity.EntityId.GetHashCode();
+		}
+	}
+
+	/*
+	 * Common adapter for getting `DetectedEntity`, because `IMyLargeTurretBase`, `IMyTurretControlBlock` and
+	 * `IMySensorBlock` do not share a common base interface to get `MyDetectedEntityInfo`.
+	 *
+	 * NOTE: Cameras are not used here, because RayCast needs to be implemented separately.
+	 */
+	class Sensor {
+
+		public static bool CanBe(IMyFunctionalBlock block) {
+			return block is IMyTurretControlBlock || block is IMyLargeTurretBase || block is IMySensorBlock;
+		}
+
+		public EntityGetter Entity;
+		public delegate MyDetectedEntityInfo EntityGetter();
+
+		public Sensor(IMyFunctionalBlock block) {
+			if (block is IMyTurretControlBlock) {
+				Entity = (block as IMyTurretControlBlock).GetTargetedEntity;
+			} else if (block is IMyLargeTurretBase) {
+				Entity = (block as IMyLargeTurretBase).GetTargetedEntity;
+			} else if (block is IMySensorBlock) {
+				Entity = () => (block as IMySensorBlock).LastDetectedEntity;
+			} else {
+				throw new ArgumentException("Block is not a Sensor!");
+			}
+		}
+	}
+
+	public Program() {
+		Runtime.UpdateFrequency = UpdateFrequency.Update1;
+
+		EntityComparer = new DetectedEntityComparer();
+		ActualEntities = new HashSet<MyDetectedEntityInfo>(EntityComparer);
+		DeprecatedEntities = new HashSet<MyDetectedEntityInfo>(EntityComparer);
+
+		BlockStorage = new List<IMyFunctionalBlock>();
+		Sensors = new List<Sensor>();
+
+		MeLCD = Me.GetSurface(0);
+		MeLCD.ContentType = VRage.Game.GUI.TextPanel.ContentType.TEXT_AND_IMAGE;
+	}
+
+	public void Main(string argument, UpdateType updateSource) {
+		UpdateSensors();
+		UpdateEntities();
+		IGC.SendBroadcastMessage(IGC_VISION, SerializeVisionEntities(), TransmissionDistance.CurrentConstruct);
+		MeLCD.WriteText(String.Join("\n", ActualEntities.ToImmutableList().ConvertAll(entity => entity.Name)));
+	}
+
+  /*
+	 * We can't to `GetBlocksOfType<IMyLargeTurretBase>` or `GetBlocksOfType<IMyTurretControlBlock>` and runtime types,
+	 * e.g. `SpaceEngineers.Game.Weapons.Guns.MyLargeGatlingTurret`, are prohibited in this context.
+	 *
+	 * But we can to `GetBlocksOfType<IMyFunctionalBlock>` and then filter them.
+	 *
+	 * Sensors can disappear as new ones appear. So we fully updating the list by checking the entire grid.
+	 */
+	void UpdateSensors() {
+		BlockStorage.Clear();
+		GridTerminalSystem.GetBlocksOfType<IMyFunctionalBlock>(BlockStorage, Sensor.CanBe);
+		Sensors.Clear();
+		Sensors.AddRange(BlockStorage.ConvertAll(block => new Sensor(block)));
+	}
+
+	List<MyDetectedEntityInfo> DetectedEntities {
+		get {
+			return Sensors
+				.ConvertAll(sensor => sensor.Entity())
+				.FindAll(entity => ! entity.IsEmpty());
+		}
+	}
+
+	void UpdateEntities() {
+		DeprecatedEntities.UnionWith(ActualEntities);
+		ActualEntities.Clear();
+		ActualEntities.UnionWith(DetectedEntities);
+		DeprecatedEntities.ExceptWith(ActualEntities);
+		// TODO: Remove Deprecated by timestamp
+	}
+
+	/*
+	 * We can't just to `SendBroadcastMessage(<tag>, Entities, <distance>)`, because:
+	 *   - `TData` is expected to be immutable
+	 *   - `MyDetectedEntityInfo` is not allowed as the `TData` inside IGC
+	 *   - a lot of C# stuff (e.g. serializers) are not allowed in this context
+	 *
+	 * Here is a solution without using handy C# features:
+	 *   - convert `HashSet` -> `ImmutableList`
+	 *   - convert `MyDetectedEntityInfo` -> `MyTuple<...>`.
+	 *
+	 * As a result: `Entities` -> `BroadcastData`, i.e. `HashSet<MyDetectedEntityInfo>` -> `ImmutableList<MyTuple<...>>`.
+	 */
+	 // TODO: DeprecatedEntities broadcast.
+	ImmutableList<
+		MyTuple<
+			MyTuple<long, string, int, Vector3D, bool, MatrixD>,
+			MyTuple<Vector3, int, BoundingBoxD, long>>>
+	SerializeVisionEntities() {
+		return ActualEntities.ToImmutableList().ConvertAll(SerializeVisionEntity);
+	}
+
+	/*
+	 * `MyDetectedEntityInfo` can't be used as the `TData` in IGC, so we convert it to 2D MyTuple. The 2D is used,
+	 * because the max tuple length is 6 while we need at least 9 to represent the `MyDetectedEntityInfo` properties + 1
+	 * to avoid the nullable `Vector3D? HitPosition` -> `Vector3D HitPosition`, `bool UsesRaycast`.
+	 */
+	MyTuple<
+		MyTuple<long, string, int, Vector3D, bool, MatrixD>,
+		MyTuple<Vector3, int, BoundingBoxD, long>>
+	SerializeVisionEntity(MyDetectedEntityInfo entity) {
+		var HitPosition = entity.HitPosition ?? new Vector3D();
+		var UsesRaycast = entity.HitPosition == null;
+		return MyTuple.Create(
+			MyTuple.Create(entity.EntityId, entity.Name, (int)entity.Type, HitPosition, UsesRaycast, entity.Orientation),
+			MyTuple.Create(entity.Velocity, (int)entity.Relationship, entity.BoundingBox, entity.TimeStamp));
+	}
+
+	#endregion // Vision
+}}
