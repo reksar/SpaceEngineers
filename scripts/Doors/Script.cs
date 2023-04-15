7bc70856485e4240c6361ef2399956a7646a3432
reksarka <reksarka@gmail.com>
2023-04-15 16:54:34 +0300

    Understable namings

--- scripts/Doors/Script.cs
+++ scripts/Doors/Script.cs
@@ -19,6 +19,12 @@ namespace Doors {
 public sealed class Program : MyGridProgram {
 #region Doors
 
+// NOTE: still 99 iterations, even after decreasing the update frequency by x10.
+const int BLOCKS_UPDATING_RATE = 99; // Iterations
+
+// --- Door Auto Close ---
+// =======================================================================================
+
 // The script will automatically close a door 1 seconds after it's being opened. Change this value here if needed:
 double autoCloseSeconds = 1;
 
@@ -56,138 +62,134 @@ double airlockDelaySeconds = 0;
 string noAirlockKeyword = "!noAirlock";
 
 
-List<IMyDoor> T = new List<IMyDoor>();
-List<IMyDoor> U = new List<IMyDoor>();
-List<IMyDoor> V = new List<IMyDoor>();
-int W = 0;
-Dictionary<IMyDoor, DateTime> X = new Dictionary<IMyDoor, DateTime>();
-Dictionary<IMyDoor, DateTime> Y = new Dictionary<IMyDoor, DateTime>();
-Dictionary<IMyDoor, IMyDoor> Z = new Dictionary<IMyDoor, IMyDoor>();
-Dictionary<IMyDoor, int> a = new Dictionary<IMyDoor, int>();
-DateTime c = new DateTime();
-int e = 0;
+int IterationCount = 0;
+int ControlledDoorsCount = 0;
+DateTime Time = new DateTime();
+List<IMyDoor> ControlledDoors = new List<IMyDoor>();
+List<IMyDoor> BrokenDoors = new List<IMyDoor>();
+List<IMyDoor> Doors = new List<IMyDoor>(); // TODO: make it local to the function?
+Dictionary<IMyDoor, DateTime> OpenDoors = new Dictionary<IMyDoor, DateTime>();
+Dictionary<IMyDoor, IMyDoor> Airlocks = new Dictionary<IMyDoor, IMyDoor>();
+Dictionary<IMyDoor, DateTime> OpenAirlocks = new Dictionary<IMyDoor, DateTime>();
+Dictionary<IMyDoor, int> OpenAirlocksPhase = new Dictionary<IMyDoor, int>();
 
 public Program() {
   Runtime.UpdateFrequency = UpdateFrequency.Update100;
 }
 
 public void Main() {
-  if (e == 0) S();
+  if (IterationCount == 0) UpdateDoors();
   if (manageAirlocks) {
-    if (e == 0) D();
-    K();
+    if (IterationCount == 0) UpdateAirlocks();
+    ManageAirlocks();
   }
-  Q();
-  c += Runtime.TimeSinceLastRun;
-  if (e >= 99) {
-    e = 0;
-  } else {
-    e++;
+  ManageDoors();
+  Time += Runtime.TimeSinceLastRun;
+  if (BLOCKS_UPDATING_RATE <= ++IterationCount) IterationCount = 0;
+}
+
+void UpdateDoors() {
+  BrokenDoors.Clear();
+  GridTerminalSystem.GetBlocksOfType(ControlledDoors, DoorToControl);
+  if (ControlledDoors.Count != ControlledDoorsCount) {
+    ControlledDoorsCount = ControlledDoors.Count;
+    OpenDoors.Clear();
+    Airlocks.Clear();
+    OpenAirlocksPhase.Clear();
   }
 }
 
-void S() {
-  V.Clear();
-  GridTerminalSystem.GetBlocksOfType(T, B);
-  if (T.Count != W) {
-    W = T.Count;
-    X.Clear();
-    Z.Clear();
-    a.Clear();
-  }
-}
-
-bool B (IMyDoor C) {
-  if (!C.IsSameConstructAs(Me)) return false;
-  if (!C.IsFunctional) {
-    V.Add(C);
+bool DoorToControl (IMyDoor door) {
+  if (!door.IsSameConstructAs(Me)) return false;
+  if (!door.IsFunctional) {
+    BrokenDoors.Add(door);
     return false;
   }
-  if (C.CustomName.Contains(manualDoorKeyword)) return false;
-  if (!autoCloseHangarDoors && C is IMyAirtightHangarDoor) return false;
+  if (door.CustomName.Contains(manualDoorKeyword)) return false;
+  if (!autoCloseHangarDoors && door is IMyAirtightHangarDoor) return false;
   return true;
 }
 
-void D() {
-  Vector3 E=new Vector3();
-  float F = 0;
-  float G = float.MaxValue;
-  int H = -1;
-  U.Clear();
-  Z.Clear();
-  U = T.FindAll(I => !(I is IMyAirtightHangarDoor));
-  foreach(var C in U) {
-    if (C.CustomName.Contains(noAirlockKeyword)) continue;
-    E = C.Position;
-    G = float.MaxValue;
-    H = -1;
-    for (int J = 0; J < U.Count; J++) {
-      if (U[J] == C) continue;
-      if (U[J].CustomName.Contains(noAirlockKeyword)) continue;
-      F = Vector3.Distance(E, U[J].Position);
-      if (F <= airlockRadius && F < G) {
-        G = F;
-        H = J;
-        if (F == 1) break;
+void UpdateAirlocks() {
+  Vector3 position = new Vector3();
+  float distance = 0;
+  float min_distance = float.MaxValue;
+  int closest_door_idx = -1;
+  Doors.Clear();
+  Airlocks.Clear();
+  Doors = ControlledDoors.FindAll(I => !(I is IMyAirtightHangarDoor));
+  foreach (var door in Doors) {
+    if (door.CustomName.Contains(noAirlockKeyword)) continue;
+    position = door.Position;
+    min_distance = float.MaxValue;
+    closest_door_idx = -1;
+    for (int i = 0; i < Doors.Count; i++) {
+      if (Doors[i] == door) continue;
+      if (Doors[i].CustomName.Contains(noAirlockKeyword)) continue;
+      distance = Vector3.Distance(position, Doors[i].Position);
+      if (distance <= airlockRadius && distance < min_distance) {
+        min_distance = distance;
+        closest_door_idx = i;
+        if (distance == 1) break;
       }
     }
-    if( H >= 0) {
-      Z[C] = U[H];
+    if (closest_door_idx >= 0) {
+      Airlocks[door] = Doors[closest_door_idx];
     }
   }
 }
 
-void K() {
-  foreach (var L in Z) {
-    IMyDoor M = L.Key;
-    IMyDoor N = L.Value;
-    bool O = Y.ContainsKey(M) ? (c - Y[M]).TotalMilliseconds >= airlockDelaySeconds * 1000 : true;
-    int P = a.ContainsKey(M) ? a[M] : 0;
+void ManageAirlocks() {
+  foreach (var door_pair in Airlocks) {
+    IMyDoor door = door_pair.Key;
+    IMyDoor sibling = door_pair.Value;
+    bool NeedToClose = OpenAirlocks.ContainsKey(door) ? (Time - OpenAirlocks[door]).TotalMilliseconds >= airlockDelaySeconds * 1000 : true;
+    int phase = OpenAirlocksPhase.ContainsKey(door) ? OpenAirlocksPhase[door] : 0;
     if (protectAirlock) {
-      if (M.Status != DoorStatus.Closed || !O || P == 1) {
-        N.Enabled = false;
+      if (door.Status != DoorStatus.Closed || !NeedToClose || phase == 1) {
+        sibling.Enabled = false;
       } else {
-         N.Enabled = true;
+        sibling.Enabled = true;
       }
     }
-    if (a.ContainsKey(N)) continue;
-    if (M.Status == DoorStatus.Open) {
-      a[M] = 1;
+    if (OpenAirlocksPhase.ContainsKey(sibling)) continue;
+    if (door.Status == DoorStatus.Open) {
+      OpenAirlocksPhase[door] = 1;
     }
-    if (a.ContainsKey(M)) {
-      if (a[M] == 1 && M.Status == DoorStatus.Closed) {
-        Y[M] = c;
-        a[M] = 2;
+    if (OpenAirlocksPhase.ContainsKey(door)) {
+      if (OpenAirlocksPhase[door] == 1 && door.Status == DoorStatus.Closed) {
+        OpenAirlocks[door] = Time;
+        OpenAirlocksPhase[door] = 2;
         continue;
       }
-      if(a[M] == 2 && O) {
-        Y.Remove(M);
-        a[M] = 3;
-        N.OpenDoor();
+      if(OpenAirlocksPhase[door] == 2 && NeedToClose) {
+        OpenAirlocks.Remove(door);
+        OpenAirlocksPhase[door] = 3;
+        sibling.OpenDoor();
       }
-      if (a[M] == 3 && N.Status == DoorStatus.Closed) {
-        a.Remove(M);
+      if (OpenAirlocksPhase[door] == 3 && sibling.Status == DoorStatus.Closed) {
+        OpenAirlocksPhase.Remove(door);
       }
     }
   }
 }
 
-void Q() {
-  foreach (var C in T) {
-    if (C.Status == DoorStatus.Open) {
-      if (!X.ContainsKey(C)) {
-        X[C] = C is IMyAdvancedDoor ? c + TimeSpan.FromSeconds(1) : c;
+void ManageDoors() {
+  foreach (var door in ControlledDoors) {
+    if (door.Status == DoorStatus.Open) {
+      if (!OpenDoors.ContainsKey(door)) {
+        OpenDoors[door] = door is IMyAdvancedDoor ? Time + TimeSpan.FromSeconds(1) : Time;
         continue;
       }
-      if (C is IMyAirtightHangarDoor) {
-        if((c - X[C]).TotalMilliseconds >= autoCloseHangarDoorsSeconds * 1000) {
-          C.CloseDoor();
-          X.Remove(C);
+      if (door is IMyAirtightHangarDoor) {
+        if((Time - OpenDoors[door]).TotalMilliseconds >= autoCloseHangarDoorsSeconds * 1000) {
+          door.CloseDoor();
+          OpenDoors.Remove(door);
         }
       } else {
-        if((c - X[C]).TotalMilliseconds >= autoCloseSeconds * 1000) {
-          C.CloseDoor();
-          X.Remove(C);
+        if((Time - OpenDoors[door]).TotalMilliseconds >= autoCloseSeconds * 1000) {
+          door.CloseDoor();
+          OpenDoors.Remove(door);
         }
       }
     }
