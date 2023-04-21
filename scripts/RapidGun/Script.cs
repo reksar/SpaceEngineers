09e632f8a98abfd91d436368523f09ba5657d779
reksarka <reksarka@gmail.com>
2023-04-21 04:09:25 +0300

    Reworked to Rotor + Piston

--- scripts/RapidGun/Script.cs
+++ scripts/RapidGun/Script.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Linq;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 // Space Engineers DLLs
 using Sandbox.ModAPI.Ingame;
@@ -23,146 +24,176 @@ public sealed class Program : MyGridProgram {
 
   #region RapidGun
 
-  //   G1 G5
-  //   G2 G6
-  //   G3 G7
-  //   G4 G8
-  //   S1 S2
-  // P__|__|
-  //    T  C
-  //
-  // `Pivot` *P* is the main piston to which 2 more are attached via conveyors.
-  IMyPistonBase Pivot;
-  // Slider *S1* is closest to the `Pivot` and is attached by a T-conveyor.
-  // Slider *S2* is attached to the free end of the T-conveyor via C-conveyor.
-  // 4 artillery guns are attached to the head of each slider. Artillery reload time is 8 seconds, so there are 8 guns
-  // *G1* .. *G8* to be able to shoot every second.
-  // Each slider with guns connected is a gun battery.
-  List<MyTuple<IMyPistonBase, List<IMyUserControllableGun>>> GunBatteries =
-    new List<MyTuple<IMyPistonBase, List<IMyUserControllableGun>>>();
-  int CurrentBatteryIdx, CurrentGunIdx;
+  // Precision to compare direction 3D vectors.
+  const double EPSILON = 0.006;
 
+  ImmutableList<Vector3I> RING_DIRECTIONS = ImmutableList.Create(new Vector3I[] {
+    Vector3I.Forward, Vector3I.Right, Vector3I.Backward, Vector3I.Left
+  });
+
+  ImmutableList<float> ANGLE_CALIBRE = ImmutableList.Create(new float[] {
+    0, MathHelper.PiOver2, MathHelper.Pi, 3 * MathHelper.PiOver2, MathHelper.TwoPi
+  });
+
+  IMyPistonBase Piston;
+  IMyMotorAdvancedStator Rotor;
+  List<List<IMyUserControllableGun>> GunRings = new List<List<IMyUserControllableGun>>();
+  IMyUserControllableGun CurrentGun;
+  int CurrentRingIdx;
   float BlockSize; // m
 
-  StringBuilder Status = new StringBuilder();
-  // To show the `Status`.
-  IMyTextSurface LCD;
-  // To `Indicate` the current state.
-  IMyTextSurface KeyLCD;
-
-	const string READY = "Arrow";
-	const string NOT_READY = "Cross";
-	const string RELOADING = "Danger";
-
   Program() {
-
-    LCD = Me.GetSurface(0);
-    LCD.ContentType = ContentType.TEXT_AND_IMAGE;
-    LCD.FontColor = Color.White;
-    LCD.BackgroundColor = Color.Black;
-
-    KeyLCD = Me.GetSurface(1);
-    KeyLCD.ContentType = ContentType.TEXT_AND_IMAGE;
-    KeyLCD.FontColor = Color.White;
-    KeyLCD.BackgroundColor = Color.Black;
-
-    AssignBlocks();
-
-    // TODO: check gun system
-    if (true) {
-      Init();
-      Runtime.UpdateFrequency = UpdateFrequency.Update10;
-    }
-  }
-
-  void AssignBlocks() {
-
-    BlockSize = Me.CubeGrid.GridSize;
-    
-    foreach (var group in MyGroups) {
-
-      var pistons = Select<IMyPistonBase>(group);
-      var sliders = pistons.FindAll(piston => IsGun(piston.TopGrid.GetCubeBlock(Vector3I.Up)));
-      Pivot = pistons.Except(sliders).FirstOrDefault();
-
-      if (Pivot == null || sliders.Count <= 0) continue;
-
-      GunBatteries.Clear();
-      sliders.ForEach(slider => GunBatteries.Add(MyTuple.Create(slider, ConnectedGuns(slider))));
-      GunBatteries.Sort(ByPivotDistance);
-      GunBatteries.Reverse();
-
-      Echo("BlockSize: " + BlockSize.ToString());
-      Echo("Sliders: " + sliders.Count.ToString());
-    }
+    SelectBlocks();
+    if (GunRings.Count > 0) Init();
   }
 
   void Main(string argument, UpdateType updateSource) {
-    var battery = GunBatteries[CurrentBatteryIdx];
-    var slider = battery.Item1;
-    var guns = battery.Item2;
-    var gun = guns[CurrentGunIdx];
-    // TODO: check Pivot position
-    if (!gun.Enabled && slider.CurrentPosition == slider.MaxLimit) {
-      gun.Enabled = true;
-    } else if (gun.IsShooting) {
-      gun.Enabled = false;
-      CurrentGunIdx++;
-      if (CurrentGunIdx >= guns.Count) {
-        CurrentGunIdx = 0;
-        slider.MaxLimit = 0;
-        slider.Velocity = -slider.MaxVelocity;
-        CurrentBatteryIdx++;
-        if (CurrentBatteryIdx >= GunBatteries.Count) {
-          CurrentBatteryIdx = 0;
-          Pivot.MaxLimit = 0;
-          Pivot.Velocity = -Pivot.MaxVelocity;
-        } else {
-          Pivot.MaxLimit += BlockSize;
-          Pivot.Velocity = Pivot.MaxVelocity;
-        }
-      } else {
-        slider.MaxLimit += BlockSize;
-        slider.Velocity = slider.MaxVelocity;
-      }
-    }
+    if (!PistonInPosition) {
+      Echo("piston");
+      if (CurrentGun != null) CurrentGun.Enabled = false;
+    } else if (!RotorInPosition) {
+      Echo("rotor");
+      if (CurrentGun != null) CurrentGun.Enabled = false;
+      Rotor.TargetVelocityRad = MathHelper.Pi; // TODO: variable
+      Rotor.RotorLock = false;
+    } else PrepareGun();
+  }
+
+  Vector3D FireDirection { get {
+    return Me.CubeGrid.WorldMatrix.Forward;
+  }}
+
+  bool PistonInPosition { get {
+    return Piston.CurrentPosition == (Piston.Velocity < 0 ? Piston.MinLimit : Piston.MaxLimit);
+  }}
+
+  bool RotorInPosition { get {
+    return Rotor.Angle == Rotor.UpperLimitRad;
+  }}
+
+  IMyUserControllableGun FindGunInCurrentFireDirection() {
+    return GunRings[CurrentRingIdx].Find(gun => gun.WorldMatrix.Forward.Equals(FireDirection, EPSILON));
+  }
+
+  void PrepareGun() {
+    Echo("stopped");
+    Rotor.RotorLock = true;
+    Rotor.TargetVelocityRad = 0;
+    if (CurrentGun == null) CurrentGun = FindGunInCurrentFireDirection();
+    if (CurrentGun == null) return; // Probably the swing amplitude is too big.
+    if (CurrentGun.IsShooting) {
+      CurrentGun.Enabled = false;
+      CurrentGun = null;
+      SetNextGun();
+    } else CurrentGun.Enabled = true;
+  }
+
+  void SetNextGun() {
+    var ready_guns = GunRings[CurrentRingIdx].FindAll(gun => !gun.IsShooting);
+    if (ready_guns.Count > 0) SetNextRotorAngle(ready_guns);
+    else SetNextRing();
+  }
+
+  void SetNextRotorAngle(List<IMyUserControllableGun> guns) {
+
+    // Precalculate this first, because it can change quickly as the grid quickly rotates.
+    var fire_direction = FireDirection;
+    var gun_directions = guns.Select(gun => gun.WorldMatrix.Forward).ToImmutableList();
+
+    // Then this will be calculated with the min delta.
+    var gun_angles = gun_directions.Select(gun_direction => {
+
+      // The direction vectors must be normalized, so the `Dot` product is enough instead of `MyMath.CosineDistance`.
+      var dot = (float)fire_direction.Dot(gun_direction);
+
+      // `dot` may be slightly out of the `cos` range due to floating point numbers, so we `Clamp` it to prevent
+      // getting the NaN from `Acos`.
+      var cos = MyMath.Clamp(dot, -1, 1);
+      return (float)CopySign(Math.Acos(cos), cos);
+    });
+
+    var closest_gun_angle = gun_angles.MinBy(Math.Abs);
+    var next_angle = AngleCalibre(Limit2Pi(closest_gun_angle + Rotor.Angle));
+    Rotor.LowerLimitRad = next_angle;
+    Rotor.UpperLimitRad = next_angle;
+  }
+
+  float AngleCalibre(float angle) {
+    var angle_calibre = ANGLE_CALIBRE.MinBy(calibre => Math.Abs(calibre - angle));
+    return angle_calibre == MathHelper.TwoPi ? 0 : angle_calibre;
+  }
+
+  void SetNextRing() {
+    // TODO:
   }
 
   void Init() {
-    const float PISTON_IMPULSE = 20000000f; // N
-    Pivot.MinLimit = 0;
-    Pivot.MaxLimit = BlockSize * GunBatteries.Count;
-    Pivot.SetValue("MaxImpulseAxis", PISTON_IMPULSE);
-    Pivot.Velocity = -Pivot.MaxVelocity;
-    GunBatteries.ForEach(battery => {
-      var slider = battery.Item1;
-      slider.MinLimit = 0;
-      slider.MaxLimit = 0;
-      slider.SetValue("MaxImpulseAxis", PISTON_IMPULSE);
-      slider.Velocity = -Pivot.MaxVelocity;
-      battery.Item2.ForEach(gun => gun.Enabled = false);
-    });
-    CurrentBatteryIdx = 0;
-    CurrentGunIdx = 0;
+
+    Runtime.UpdateFrequency = UpdateFrequency.Update10;
+
+    BlockSize = Me.CubeGrid.GridSize;
+
+    Piston.MinLimit = 0;
+    Piston.MaxLimit = 0;
+    Piston.Velocity = -Piston.MaxVelocity;
+
+    Rotor.TargetVelocityRad = 0;
+    Rotor.Displacement = -0.3f; // m
+    Rotor.Torque = 2000000; // N*m
+    Rotor.BrakingTorque = 500000000; // N*m
+    Rotor.LowerLimitRad = 0;
+    Rotor.UpperLimitRad = 0;
+    Rotor.RotorLock = true;
+
+    GunRings.ForEach(ring => ring.ForEach(gun => gun.Enabled = false));
+    CurrentRingIdx = 0;
+    CurrentGun = null;
   }
 
-  void Indicate(string image) {
-		KeyLCD.ClearImagesFromSelection();
-		KeyLCD.AddImageToSelection(image);
+   void SelectBlocks() {
+
+    var my_groups = new List<IMyBlockGroup>();
+    GridTerminalSystem.GetBlockGroups(my_groups, group =>
+      Select<IMyProgrammableBlock>(group, block => block == Me).Count == 1);
+
+    var piston_and_rotor = my_groups
+      .Select(group => MyTuple.Create(
+        Select<IMyPistonBase>(group).FirstOrDefault(),
+        Select<IMyMotorAdvancedStator>(group).FirstOrDefault()
+      ))
+      .FirstOrDefault(tuple => tuple.Item1 != null && tuple.Item2 != null);
+
+    Piston = piston_and_rotor.Item1;
+    Rotor = piston_and_rotor.Item2;
+
+    SelectGuns();
   }
 
-  void ShowStatus() {
-    LCD.WriteText(Status);
-    Echo(Status.ToString());
+  void SelectGuns() {
+
+    if (Rotor == null) return;
+
+    var grid = Rotor.TopGrid;
+    var axis = Vector3I.Up;
+    var guns = GunRing(grid, axis);
+
+    while (guns.Count > 0) {
+      GunRings.Add(guns);
+      axis += Vector3I.Up;
+      guns = GunRing(grid, axis);
+    }
+
+    GunRings.Reverse();
   }
 
-  List<IMyBlockGroup> MyGroups { get {
-    var groups = new List<IMyBlockGroup>();
-    GridTerminalSystem.GetBlockGroups(groups, group =>
-      Select<IMyProgrammableBlock>(group, block =>
-        block == Me).Count == 1);
-    return groups;
-  }}
+  List<IMyUserControllableGun> GunRing(IMyCubeGrid grid, Vector3I axis) {
+    return RING_DIRECTIONS
+      .Select(direction => grid.GetCubeBlock(axis + direction))
+      .Where(block => block != null)
+      .Select(block => block.FatBlock as IMyUserControllableGun)
+      .Where(gun => gun != null)
+      .ToList();
+  }
 
   List<T> Select<T>(IMyBlockGroup group, Func<T, bool> filter = null) where T : class {
 		var blocks = new List<T>();
@@ -170,39 +201,24 @@ public sealed class Program : MyGridProgram {
 		return blocks;
 	}
 
-  List<IMyUserControllableGun> ConnectedGuns(IMyPistonBase slider) {
-
-    var guns = new List<IMyUserControllableGun>();
-    var grid = slider.TopGrid;
-
-    var position = Vector3I.Up;
-    var block = grid.GetCubeBlock(position);
-
-    while (IsGun(block)) {
-      guns.Add(block.FatBlock as IMyUserControllableGun);
-
-      position += Vector3I.Up;
-      block = grid.GetCubeBlock(position);
-    }
-
-    guns.Sort((gun1, gun2) => gun1.Position.Y - gun2.Position.Y);
-    guns.Reverse();
-    return guns;
+  /*
+   * Returns radians in the range [0 .. 2Pi].
+   * NOTE: similar functions from `MathHelper` do not work!
+   */
+  float Limit2Pi(float radians) {
+    return radians < 0 ? (radians % MathHelper.TwoPi + MathHelper.TwoPi) : (radians % MathHelper.TwoPi);
   }
 
-  bool IsGun(IMySlimBlock block) {
-    return null != block && block.FatBlock is IMyUserControllableGun;
+  /*
+   * Returns a value with the `magnitude` and the `sign`.
+   * NOTE: `Math.CopySign` is not allowed inside a script!
+   */
+  double CopySign(double magnitude, double sign) {
+    return IsNegative(magnitude) == IsNegative(sign) ? magnitude : -magnitude;
   }
 
-  int ByPivotDistance(
-    MyTuple<IMyPistonBase, List<IMyUserControllableGun>> battery1,
-    MyTuple<IMyPistonBase, List<IMyUserControllableGun>> battery2
-  ) {
-    var slider1 = battery1.Item1;
-    var slider2 = battery2.Item1;
-    var distance1 = Vector3.Distance(Pivot.GetPosition(), slider1.GetPosition());
-    var distance2 = Vector3.Distance(Pivot.GetPosition(), slider2.GetPosition());
-    return (int)(distance1 - distance2);
+  bool IsNegative(double value) {
+    return Math.Sign(value) == -1;
   }
 
   #endregion // RapidGun
