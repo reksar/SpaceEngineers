3e11f7d0037d704af95a151cb03e8e85efe3d690
reksarka <reksarka@gmail.com>
2023-04-23 00:32:05 +0300

    More rotor stability and some fixes

--- scripts/RapidGun/Script.cs
+++ scripts/RapidGun/Script.cs
@@ -24,13 +24,14 @@ public sealed class Program : MyGridProgram {
 
   #region RapidGun
 
-  // Precision to compare direction 3D vectors.
+  // Precision to compare the direction of 3D vectors.
   const double EPSILON = 0.006;
 
   ImmutableList<Vector3I> RING_DIRECTIONS = ImmutableList.Create(new Vector3I[] {
     Vector3I.Forward, Vector3I.Right, Vector3I.Backward, Vector3I.Left
   });
 
+  // 0°, 90°, 180°, 270°, 360°
   ImmutableList<float> ANGLE_CALIBRE = ImmutableList.Create(new float[] {
     0, MathHelper.PiOver2, MathHelper.Pi, 3 * MathHelper.PiOver2, MathHelper.TwoPi
   });
@@ -42,21 +43,31 @@ public sealed class Program : MyGridProgram {
   int CurrentRingIdx;
   float BlockSize; // m
 
+  StringBuilder Status = new StringBuilder();
+  IMyTextSurface LCD;
+  string SavedStatus = "";
+
   Program() {
+
+		LCD = Me.GetSurface(0);
+		LCD.ContentType = ContentType.TEXT_AND_IMAGE;
+    LCD.BackgroundColor = Color.Black;
+
     SelectBlocks();
+
     if (GunRings.Count > 0) Init();
   }
 
   void Main(string argument, UpdateType updateSource) {
-    if (!PistonInPosition) {
-      Echo("piston");
-      if (CurrentGun != null) CurrentGun.Enabled = false;
-    } else if (!RotorInPosition) {
-      Echo("rotor");
-      if (CurrentGun != null) CurrentGun.Enabled = false;
-      Rotor.TargetVelocityRad = MathHelper.Pi; // TODO: variable
-      Rotor.RotorLock = false;
-    } else PrepareGun();
+
+    Status.Clear();
+
+    if (!PistonInPosition) Slide();
+    else if (!RotorInPosition) Rotate();
+    else PrepareGun();
+
+    Status.Append(SavedStatus);
+    LCD.WriteText(Status);
   }
 
   Vector3D FireDirection { get {
@@ -75,8 +86,41 @@ public sealed class Program : MyGridProgram {
     return GunRings[CurrentRingIdx].Find(gun => gun.WorldMatrix.Forward.Equals(FireDirection, EPSILON));
   }
 
+  void Slide() {
+    Status.Append("Sliding ...\n");
+    if (CurrentGun != null) CurrentGun.Enabled = false;
+    // TODO:
+  }
+
+  void Rotate() {
+    Status.Append("Rotating: ");
+    if (CurrentGun != null) CurrentGun.Enabled = false;
+    Rotor.TargetVelocityRad = TargetVelocity();
+    Rotor.RotorLock = false;
+  }
+
+  float TargetVelocity() {
+
+    // NOTE: `Rotor.Angle` can exceed 2π more than 2 times, but it can't be seen in the in-game rotor properties!
+    var current_angle = Limit2Pi(Rotor.Angle);
+
+    var delta = Rotor.UpperLimitRad - current_angle;
+    if (delta < -MathHelper.Pi) delta += MathHelper.TwoPi;
+
+    Status.Append(Math.Round(Rotor.UpperLimitRad, 2)+" - "+Math.Round(current_angle, 2)+" = ");
+    Status.Append(Math.Round(delta, 2)+" rad\n");
+
+    var k = (float)Math.Sin(delta * delta / MathHelper.TwoPi);
+
+    // TODO: move
+    Rotor.Torque = 1000000000 * Math.Abs(k);
+    Rotor.BrakingTorque = 1000000000 - Rotor.Torque - 40000;
+
+    // NOTE: max rotor velocity is ±π rad/s
+    return MathHelper.Pi * k * 3;
+  }
+
   void PrepareGun() {
-    Echo("stopped");
     Rotor.RotorLock = true;
     Rotor.TargetVelocityRad = 0;
     if (CurrentGun == null) CurrentGun = FindGunInCurrentFireDirection();
@@ -85,7 +129,10 @@ public sealed class Program : MyGridProgram {
       CurrentGun.Enabled = false;
       CurrentGun = null;
       SetNextGun();
-    } else CurrentGun.Enabled = true;
+    } else {
+      Status.Append("Ready: "+Math.Round(Rotor.Angle, 2)+" rad\n");
+      CurrentGun.Enabled = true;
+    }
   }
 
   void SetNextGun() {
@@ -100,7 +147,7 @@ public sealed class Program : MyGridProgram {
     var fire_direction = FireDirection;
     var gun_directions = guns.Select(gun => gun.WorldMatrix.Forward).ToImmutableList();
 
-    // Then this will be calculated with the min delta.
+    // Then this will be calculated with the min deviation.
     var gun_angles = gun_directions.Select(gun_direction => {
 
       // The direction vectors must be normalized, so the `Dot` product is enough instead of `MyMath.CosineDistance`.
@@ -109,18 +156,48 @@ public sealed class Program : MyGridProgram {
       // `dot` may be slightly out of the `cos` range due to floating point numbers, so we `Clamp` it to prevent
       // getting the NaN from `Acos`.
       var cos = MyMath.Clamp(dot, -1, 1);
-      return (float)CopySign(Math.Acos(cos), cos);
+
+      var angle = (float)Math.Acos(cos);
+
+      return cos < 0 ? MathHelper.TwoPi - angle : angle; 
+    }).ToArray();
+
+    var closest_gun_angle = gun_angles.MinBy(angle => {
+      var delta = Math.Abs(Rotor.Angle - angle);
+      // Ideally `delta == 0 ?`, but floating point numbers cause problems.
+      return delta < MathHelper.PiOver4 ? MathHelper.TwoPi : delta;
     });
 
-    var closest_gun_angle = gun_angles.MinBy(Math.Abs);
-    var next_angle = AngleCalibre(Limit2Pi(closest_gun_angle + Rotor.Angle));
-    Rotor.LowerLimitRad = next_angle;
-    Rotor.UpperLimitRad = next_angle;
+    var next_angle_raw = Rotor.Angle + Math.Abs(Rotor.Angle - closest_gun_angle);
+    var next_angle_limited = Limit2Pi(next_angle_raw);
+
+    var next_angle = AngleCalibre(next_angle_limited);
+
+    Status.Append("\nSelected Gun: "+Math.Round(closest_gun_angle, 2)+" << ");
+    foreach (var angle in gun_angles) Status.Append(Math.Round(angle, 2)+" ");
+    Status.Append("\nSelected Angle: "+Math.Round(next_angle, 2)+"\n");
+
+    Status.Append("Raw: "+next_angle_raw+"\n");
+    Status.Append("Limited: "+next_angle_limited+"\n");
+
+    SavedStatus = Status.ToString();
+
+    SetRotorLimitRad(next_angle);
   }
 
-  float AngleCalibre(float angle) {
-    var angle_calibre = ANGLE_CALIBRE.MinBy(calibre => Math.Abs(calibre - angle));
-    return angle_calibre == MathHelper.TwoPi ? 0 : angle_calibre;
+  void SetRotorLimitRad(float radians) {
+    if (radians < Rotor.LowerLimitRad) {
+      Rotor.LowerLimitRad = radians;
+      Rotor.UpperLimitRad = radians;
+    } else {
+      Rotor.UpperLimitRad = radians;
+      Rotor.LowerLimitRad = radians;
+    }
+  }
+
+  float AngleCalibre(float radians) {
+    var angle = ANGLE_CALIBRE.MinBy(calibre => Math.Abs(calibre - radians));
+    return angle < MathHelper.TwoPi ? angle : 0;
   }
 
   void SetNextRing() {
@@ -141,8 +218,7 @@ public sealed class Program : MyGridProgram {
     Rotor.Displacement = -0.3f; // m
     Rotor.Torque = 2000000; // N*m
     Rotor.BrakingTorque = 500000000; // N*m
-    Rotor.LowerLimitRad = 0;
-    Rotor.UpperLimitRad = 0;
+    SetRotorLimitRad(0);
     Rotor.RotorLock = true;
 
     GunRings.ForEach(ring => ring.ForEach(gun => gun.Enabled = false));
@@ -203,7 +279,7 @@ public sealed class Program : MyGridProgram {
 
   /*
    * Returns radians in the range [0 .. 2Pi].
-   * NOTE: similar functions from `MathHelper` do not work!
+   * NOTE: similar function from `MathHelper` do not work!
    */
   float Limit2Pi(float radians) {
     return radians < 0 ? (radians % MathHelper.TwoPi + MathHelper.TwoPi) : (radians % MathHelper.TwoPi);
@@ -213,7 +289,7 @@ public sealed class Program : MyGridProgram {
    * Returns a value with the `magnitude` and the `sign`.
    * NOTE: `Math.CopySign` is not allowed inside a script!
    */
-  double CopySign(double magnitude, double sign) {
+  float CopySign(float magnitude, float sign) {
     return IsNegative(magnitude) == IsNegative(sign) ? magnitude : -magnitude;
   }
 
