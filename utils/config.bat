d0e78db8b9ea13af13129b17e0eadce08920ab8d
reksarka <reksarka@gmail.com>
2024-05-16 09:02:44 +0300

    Uses the `git.path` setting if exists

--- utils/config.bat
+++ utils/config.bat
@@ -1,14 +1,23 @@
 @echo off
 
-set SE_DIR=%userprofile%\AppData\Roaming\SpaceEngineers
-set SE_SCRIPTS_DIR=%SE_DIR%\IngameScripts\local
+set "SE_DIR=%userprofile%\AppData\Roaming\SpaceEngineers"
+set "SE_SCRIPTS_DIR=%SE_DIR%\IngameScripts\local"
 set CS=Script.cs
 set PNG=thumb.png
 
 set git_dir=
 
-for /f "usebackq delims=" %%i in (`where git 2^>NUL`) do (
-  set git_dir=%%~dpi..
+if exist "%git_path%" (
+  rem  Use `git.path` from `settings.json`, which is set as the %git_path% env
+  rem  var in `tasks.json`.
+  for %%i in ("%git_path%") do (
+    set "git_dir=%%~dpi.."
+  )
+) else (
+  rem  Trying to use a system Git path.
+  for /f "usebackq delims=" %%i in (`where git 2^>NUL`) do (
+    set "git_dir=%%~dpi.."
+  )
 )
 
 if "%git_dir%" == "" (
@@ -16,7 +25,7 @@ if "%git_dir%" == "" (
   exit /b 1
 )
 
-set sed=%git_dir%\usr\bin\sed.exe
+set "sed=%git_dir%\usr\bin\sed.exe"
 
 if not exist "%sed%" (
   echo Not found: "%sed%".
